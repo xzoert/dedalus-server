3db691a84ba42f37548ea726d7e70c3f13a0edb9
xzoert <max.jacob@alternalab.it>
2016-11-03 11:55:12 +0100

    removed inotify

--- server.js
+++ server.js
@@ -1,69 +1,7 @@
 var http = require("http");
 var Tagman=require('tagman');
-var Inotify=require('inotify').Inotify;
 const fs=require('fs');
 
-/*
-fs.watch('/home/xzoert', (event,fn) => {
-	console.log('WATCH:',event,fn);
-});
-*/
-/*
-fs.watchFile('/home/xzoert/test/pluto.txt',(cur,prev) => {
-	console.log('CUR',cur,'PREV',prev);
-});
-*/
-
-/*
-var callback = function(event) {
-	var mask = event.mask;
-	var type = mask & Inotify.IN_ISDIR ? 'directory ' : 'file ';
-	event.name ? type += ' ' + event.name + ' ': ' ';
-
-	//the porpuse of this hell of 'if' 
-	//statements is only illustrative. 
-
-	if(mask & Inotify.IN_ACCESS) {
-		console.log(type + 'was accessed ');
-	} else if(mask & Inotify.IN_MODIFY) {
-		console.log(type + 'was modified ');
-	} else if(mask & Inotify.IN_OPEN) {
-		console.log(type + 'was opened ');
-	} else if(mask & Inotify.IN_CLOSE_NOWRITE) {
-		console.log(type + ' opened for reading was closed ');
-	} else if(mask & Inotify.IN_CLOSE_WRITE) {
-		console.log(type + ' opened for writing was closed ');
-	} else if(mask & Inotify.IN_ATTRIB) {
-		console.log(type + 'metadata changed ');
-	} else if(mask & Inotify.IN_CREATE) {
-		console.log(type + 'created');
-	} else if(mask & Inotify.IN_DELETE) {
-		console.log(type + 'deleted');
-	} else if(mask & Inotify.IN_DELETE_SELF) {
-		console.log(type + 'watched deleted ');
-	} else if(mask & Inotify.IN_MOVE_SELF) {
-		console.log(type + 'watched moved');
-		console.log(event);
-	} else if(mask & Inotify.IN_IGNORED) {
-		console.log(type + 'watch was removed');
-	} else if(mask & Inotify.IN_MOVED_FROM) {
-		data = event;
-		data.type = type;
-	} else if(mask & Inotify.IN_MOVED_TO) {
-		if( Object.keys(data).length &&
-			data.cookie === event.cookie) {
-			console.log(type + ' moved to ' + data.type);
-			data = {};
-		}
-	}
-}
-var inotify=new Inotify();
-var watcher = inotify.addWatch({
-	path: '/home/xzoert',
-	watch_for: Inotify.IN_ALL_EVENTS,
-	callback: callback
-});
-*/
 
 process.on('uncaughtException', (err) => {
 	console.log('ERROR',err)
